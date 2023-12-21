dcb146a6149b9ba8e729f87342894a208bc8e4d7
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-21 08:32:05 +0600

    class and object

--- Class_Object.php
+++ Class_Object.php
@@ -1,8 +1,7 @@
 <?php
-#========================class and object=================
 #class ClassName{}
 
-class Foo{
+/*class Foo{
     public $color;
 
     function display(){
@@ -22,5 +21,35 @@ $obj = new Foo();
 $obj->display();
 $obj->color = "Red";
 echo $obj->color. '<br>';
-$obj->getColor();
+$obj->getColor();*/
+
+?>
+<?php
+#=======================Class Properties and Methods======================
+class Vehicle{
+    public $name = "Corola";
+    public $color = "Red";
+
+    function set($name, $color){
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    function display(){
+        echo "Name is : {$this->name} <br>";
+        echo "Color is : {$this->color} <br>";
+    }
+   
+}
+
+$vehicle = new Vehicle();
+
+//$vehicle->name = " Bus";
+//$vehicle->color=" Blue";
+//echo "Name is :".$vehicle->name."<br>";
+//echo "Color is :".$vehicle->color."<br>";
+
+$vehicle->set("BMW", "White");
+$vehicle->display();
+
 ?>
