d29ebd4915e3f341da116839fb4b8a83943879f8
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-23 11:40:34 +0600

    abstract_class

---
+++ abstract_class.php
@@ -0,0 +1,36 @@
+<?php
+
+    abstract class User
+    {
+        public abstract function getName();
+        public abstract function display();
+    }
+
+    class Student extends User
+    {
+        public $name = "Jone Doe";
+
+        public function getName()
+        {
+            return $this->name;
+        }
+        public function display(){
+            echo "Welcome to display <br>";
+        }
+
+    }
+$std = new Student;
+$std->display();
+echo $std->getName();
+
+
+
+
+
+
+
+
+
+
+
+?>
